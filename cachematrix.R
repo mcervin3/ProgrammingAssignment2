92607746f04ab795769465cc2ebd37949f2818bf
mcervin3 <mcervin3@gmail.com>
2015-02-19 18:28:05 -0500

    Completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This file contains two functions. The first function, makeCacheMatrix, takes
+## in a matrix and returns a list of functions which can thereafter be used to
+## get or set the matrix object, or get or set the inverse of the matrix object.
+## The second function will solve for the inverse of the special matrix object
+## created by the first function - or if the inverse has already been solved,
+## it will fetch that inverse from a cached variable.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes in a matrix and returns a list of functions which can
+## thereafter be used to get or set the matrix object, or get or set the inverse
+## of the matrix object. Note: this makes no check that the matrix is square
+## (and therefore invertible).
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(solve) m <<- solve
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve will solve for the inverse of the special matrix object
+## created by the first function - or if the inverse has already been solved,
+## it will fetch that inverse from a cached variable. Note: there is no check
+## to guarantee that the matrix is square (and therefore invertible).
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
